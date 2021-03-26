b4065fa034c53e36e87ae925725cdf65b9e288ea
Marady <maradyalexis@gmail.com>
2021-03-26 00:54:51 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 function addingEventListener() {
-}
+    const input = document.getElementById('input');
+    function clickAlert() {   
+      alert('Cheers!');
+    }
+    
+    input.addEventListener('click', clickAlert); 
+      } 
+
+const input = document.getElementById('input');
+      input.addEventListener('click', function() {
+        alert('Cheers!');
+      });      
